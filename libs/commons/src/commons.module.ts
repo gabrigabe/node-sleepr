6bb675e607ebbf3fec3af725ea5449b5d8e73f2a
root <root@DESKTOP-DAR7GTC>
2023-07-04 22:29:45 -0300

    feat: add config and database module for commons

--- libs/commons/src/commons.module.ts
+++ libs/commons/src/commons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonsService } from './commons.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   providers: [CommonsService],
   exports: [CommonsService],
+  imports: [DatabaseModule, ConfigModule],
 })
 export class CommonsModule {}
